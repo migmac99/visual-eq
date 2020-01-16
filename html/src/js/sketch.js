f389cb3b6ba306bfc1f9099c040db33ae1bac0b8
migmac99 <migmacthegamer@gmail.com>
2020-01-16 02:35:06 +0000

    Initial Commit

---
+++ html/src/js/sketch.js
@@ -0,0 +1,63 @@
+let mic, fft, bg;
+
+let w; //width of each band
+let smooth = 0.9; //smoothing value (0 - 1)
+let bands = 2048; //number of bands
+
+function preload() {
+    bg = loadImage('image.png');
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    image(bg, 0, 0);
+
+    colorMode(HSB);
+    angleMode(DEGREES);
+
+    mic = new p5.AudioIn();
+    mic.start();
+    fft = new p5.FFT(smooth, bands);
+    fft.setInput(mic);
+    w = width / bands;
+    ww = -width / bands;
+}
+
+function draw() {
+    // document.querySelectorAll("p5Canvas").forEach(element => element.style.backgroundImage = "url(image.png)");
+    background(bg);
+    var spectrum = fft.analyze();
+    // console.log(spectrum);
+
+    stroke(255);
+    // noStroke();
+
+    // Left Side
+    for (var i = 0; i < spectrum.length; i++) {
+        var amp = spectrum[i];
+        var y = map(amp, 0, 256, height, 0);
+
+        // fill(i, 255, 255);
+        // rect(i * w, y, w, height - i);
+
+        line(i * w, height, i * w, y);
+    }
+
+    // Right Side
+    for (var i = 0; i < spectrum.length; i++) {
+        var amp = spectrum[i];
+        var y = map(amp, 0, 256, height, 0);
+
+        // fill(i, 255, 255);
+        // rect(i * w, y, w, height - i);
+
+        line(i * ww + width, height, i * ww + width, y);
+    }
+}
+
+function touchStarted() {
+    if (getAudioContext().state !== 'running') {
+        getAudioContext().resume();
+    }
+}
